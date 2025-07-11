e725dbe2edd2dd6d372a0bb144b1bb2e68545205
Marcelo Fernandes <marcelofernandes.mga@gmail.com>
2025-07-11 14:58:50 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,1 +1,15 @@
+import { defineConfig } from 'astro/config';
 
+export default defineConfig({
+  site: 'https://your-username.github.io',
+  base: '/f1-racing-landing',
+  output: 'static',
+  build: {
+    assets: 'assets'
+  },
+  vite: {
+    build: {
+      assetsInlineLimit: 0
+    }
+  }
+});
